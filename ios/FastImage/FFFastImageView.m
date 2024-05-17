5a0e965b964aa2f0d39f527795fabc72002aea10
vatsalredspark <104831176+vatsalredspark@users.noreply.github.com>
2024-05-17 11:37:32 +0530

    Update FFFastImageView.m
    Changed makeImage function because of white screen issue in iOS 17+

--- ios/FastImage/FFFastImageView.m
+++ ios/FastImage/FFFastImageView.m
@@ -71,13 +71,33 @@
     }
 }
 
-- (UIImage*) makeImage: (UIImage*)image withTint: (UIColor*)color {
+(* - (UIImage*) makeImage: (UIImage*)image withTint: (UIColor*)color {
     UIImage* newImage = [image imageWithRenderingMode: UIImageRenderingModeAlwaysTemplate];
     UIGraphicsBeginImageContextWithOptions(image.size, NO, newImage.scale);
     [color set];
     [newImage drawInRect: CGRectMake(0, 0, image.size.width, newImage.size.height)];
     newImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
+    return newImage;
+} *)
+
+- (UIImage*)makeImage:(UIImage *)image withTint:(UIColor *)color {
+    UIImage *newImage = [image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    if (@available(iOS 10.0, *)) {
+        UIGraphicsImageRenderer *renderer = [[UIGraphicsImageRenderer alloc] initWithSize:image.size];
+        newImage = [renderer imageWithActions:^(UIGraphicsImageRendererContext * _Nonnull rendererContext) {
+            [color setFill];
+            [newImage drawInRect:CGRectMake(0, 0, image.size.width, newImage.size.height)];
+        }];
+    } else {
+        // Fallback on earlier versions
+        UIGraphicsBeginImageContextWithOptions(image.size, NO, 0);
+            CGContextRef context = UIGraphicsGetCurrentContext();
+            [color setFill];
+            CGContextFillRect(context, CGRectMake(0, 0, image.size.width, image.size.height));
+            newImage = UIGraphicsGetImageFromCurrentImageContext();
+            UIGraphicsEndImageContext();
+    }
     return newImage;
 }
 
